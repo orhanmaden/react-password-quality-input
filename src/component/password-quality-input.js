d8475f5d95cdfffa8946e33d786d3d2e9989592c
orhanmaden <orhanmaden@gmail.com>
2016-01-14 21:51:36 +0100

    Updated class name to match the file name.

--- src/component/password-quality-input.js
+++ src/component/password-quality-input.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import zxcvbn from 'zxcvbn'
 
-export default class PasswordQuality extends React.Component {
+export default class PasswordQualityInput extends React.Component {
   
   constructor(props) {
     super(props);
